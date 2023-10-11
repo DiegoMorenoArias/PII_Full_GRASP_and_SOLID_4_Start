7cc0d51af29947bcb7fff43ee1ff06c325051a9a
Diego Moreno <diego.morenoa@correo.ucu.edu.uy>
2023-10-11 10:14:58 -0300

    Ejercicio terminado poniéndole las cosas necesarias para que use el Creator

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -15,9 +15,9 @@ namespace Full_GRASP_And_SOLID
 
         public Product FinalProduct { get; set; }
 
-        public void AddStep(Step step)
+        public void AddStep(Product input, double quantity, Equipment equipment, int time) // Recibe todos los parámetros necesarios para crear el Step
         {
-            this.steps.Add(step);
+            this.steps.Add(new Step(input, quantity, equipment, time)); // Se crea el step acá
         }
 
         public void RemoveStep(Step step)
